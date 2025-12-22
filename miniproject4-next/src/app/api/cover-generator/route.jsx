468a5e54249c538821a3d9ec9ba956ab3fa99ae8
kevincms <kevincms314@naver.com>
2025-12-22 14:02:03 +0900

    test

--- miniproject4-next/src/app/api/cover-generator/route.jsx
+++ miniproject4-next/src/app/api/cover-generator/route.jsx
@@ -4,7 +4,7 @@
 import { NextResponse } from 'next/server';
 // 스프링 백엔드 주소
 //const API_BASE_URL =
-    process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
+//   process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:8080";
 
 const API_BASE_URL = "http://10.99.2.11:8080"
 // OpenAI SDK가 없다고 가정하고, Node.js의 fetch를 사용한 기본적인 로직만 구현합니다.
